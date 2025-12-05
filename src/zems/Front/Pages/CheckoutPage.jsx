28b4b218546d18f5862d2468bf4dee62cffc2df5
dev-nirob1 <dev.nirob1@gmail.com>
2025-12-05 22:14:46 +0600

    update

--- src/zems/Front/Pages/CheckoutPage.jsx
+++ src/zems/Front/Pages/CheckoutPage.jsx
@@ -54,6 +54,97 @@ const CheckoutPage = () => {
               </form>
             </div>
           </div>
+
+          <div className="border border-gray-100 rounded p-3">
+            <table className="col-span-4 w-full border border-gray-50 mb-6">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="py-2 text-left">Product</th>
+                  <th className="py-2 text-left">Price</th>
+                  <th className="py-2 text-left">Quantity</th>
+                  <th className="py-2 text-left">Total</th>
+                  <th className="py-2 text-left">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="not-last:border-b not-last:border-b-gray-50">
+                  <td className="py-2 flex items-center gap-4">
+                    <img
+                      className="h-16 w-16"
+                      src="https://images.unsplash.com/photo-1740711152088-88a009e877bb?q=80&w=580&auto=format&fit=crop"
+                      alt=""
+                    />
+                    <div>
+                      <h5 className="font-semibold">Classic Cotton T-Shirt</h5>
+                      <p className="text-sm text-gray-500">Size: L</p>
+                    </div>
+                  </td>
+
+                  <td>৳ 1400</td>
+
+                  <td>
+                    <div className="flex items-center">
+                      <button className="px-3 py-1 bg-neutral-900 text-white">
+                        -
+                      </button>
+                      <span className="px-4 py-1 bg-gray-200">0</span>
+                      <button className="px-3 py-1 bg-neutral-900 text-white">
+                        +
+                      </button>
+                    </div>
+                  </td>
+
+                  <td>৳ 1400</td>
+
+                  <td>
+                    <button className="bg-red-500 rounded text-white px-2 py-1">
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            {/* cart total price  */}
+            <div className="data p-4 border border-gray-100">
+              <div className="flex justify-between py-2">
+                <h5 className="font-semibold text-neutral-800">SubTotal</h5>
+                <p className="font-bold text-neutral-700">৳ 1400</p>
+              </div>
+              <div className="flex justify-between py-2">
+                <h5 className="font-semibold text-neutral-800">
+                  Deliver Charge
+                </h5>
+                <p className="font-bold text-neutral-700">৳ 120</p>
+              </div>
+              {/* total  */}
+              <div className="flex justify-between py-2 border-t border-t-gray-100">
+                <h5 className="font-semibold text-neutral-800">Total</h5>
+                <p className="font-bold text-neutral-700">৳ 1520</p>
+              </div>
+
+              {/* payment method */}
+
+              <div className="space-y-3 mt-3">
+                <div className="flex items-center gap-2 p-3 border border-gray-100">
+                  <input type="radio" name="payment" id="bkash" />{" "}
+                  <label For="bkash">Bkash</label>
+                </div>
+                <div className="flex items-center gap-2 p-3 border border-gray-100">
+                  <input type="radio" name="payment" id="nagod" />{" "}
+                  <label For="nogod">Nagod</label>
+                </div>
+                <div className="flex items-center gap-2 p-3 border border-gray-100">
+                  <input type="radio" name="payment" id="rocket" />{" "}
+                  <label For="rocket">Rocket</label>
+                </div>
+              </div>
+
+              <button className="p-2 mt-4 cursor-pointer rounded bg-neutral-900 text-white font-semibold w-full">
+                Order Now
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
